daeaf63684fccdbafb1dc73870085dcb0925272a
Silas Burger <ybsilas@gmail.com>
2025-05-10 16:58:50 +0200

    mini.move
    Signed-off-by: Silas Burger <ybsilas@gmail.com>

--- init.lua
+++ init.lua
@@ -768,10 +768,11 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        go = { 'goimports', 'gofmt' },
       },
     },
   },
@@ -917,6 +918,7 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+      require('mini.move').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
